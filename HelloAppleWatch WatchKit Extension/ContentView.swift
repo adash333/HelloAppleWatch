9cc6f8d7cfe36ab0c8bca7a04a4d27ce7d64fa64
adash333 <a@MBA.local>
2021-10-03 19:51:31 +0900

    first commit

---
+++ HelloAppleWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloAppleWatch WatchKit Extension
+//
+//  Created by a on 2021/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloAppleWatch WatchKit Extension/HelloAppleWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  HelloAppleWatchApp.swift
+//  HelloAppleWatch WatchKit Extension
+//
+//  Created by a on 2021/09/27.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
